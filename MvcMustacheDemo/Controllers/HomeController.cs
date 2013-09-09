1049865d42715e7c1eba92bb01a29620b0031147
itorian <abhikumarvatsa@yahoo.co.in>
2013-09-09 16:15:50 +0530

    initialcommit

---
+++ MvcMustacheDemo/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using MvcMustacheDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMustacheDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult MyFriends()
+        {
+            List<Friend> friends = new List<Friend>();
+            friends.Add(new Friend { Name = "Deepak", Address = "Bokaro", Mobile = "9966453241" });
+            friends.Add(new Friend { Name = "Rohit", Address = "Dhanbad", Mobile = "5454544533" });
+            friends.Add(new Friend { Name = "Ram", Address = "Dumka", Mobile = "4456576589" });
+            friends.Add(new Friend { Name = "Shyam", Address = "Ranchi", Mobile = "9876543456" });
+            friends.Add(new Friend { Name = "Mohan", Address = "Giridih", Mobile = "9876567439" });
+
+            return Json(friends, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ MvcMustacheDemo/Models/Friend.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcMustacheDemo.Models
+{
+    public class Friend
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Mobile { get; set; }
+    }
+}
